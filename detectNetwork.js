a4fa91bfc5d3e3bf322c11ad50630d6b3bff7961
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-06-23 14:11:55 -0500

    deleted Maestro functionality

--- detectNetwork.js
+++ detectNetwork.js
@@ -10,8 +10,6 @@
 
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   //helper function for an American Express card
@@ -32,7 +30,6 @@ var detectNetwork = function(cardNumber) {
   if(isVisa(cardNumber[0], cardNumber.length)) {
     return 'Visa';
   }
-
 
   //get first four digits for Discover & Maestro prefix
   var prefix = cardNumber.substring(0, 4);
@@ -61,26 +58,11 @@ var detectNetwork = function(cardNumber) {
     return 'Discover';
   }
 
-  //helper function for a Maestro
-    //prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
-  var isMaestro = function (prefix, length) {
-    //first test for length
-    if (length >= 12 && length <= 19) {
-      return prefix === '5018' || prefix === '5020' || prefix === '5038' || prefix === '6304';
-    }
-  }
-
-  //test for Maestro, reminder: prefix was already set around line 64
-  if(isMaestro(prefix, cardNumber.length)) {
-    return 'Maestro';
-  }
-
   //modify prefix to be first six digits for Union Pay
   prefix = parseInt(cardNumber.substring(0, 6));
 
   //helper function for China UnionPay
     //prefix of 622126-622925, 624-626, or 6282-6288 and a length of 16-19.
-    //622126-622925, 624000-626999, 628200-628899
   var isUnionPay = function (prefix, length) {
     //first test for length
     if (length >= 16 && length <= 19) {
